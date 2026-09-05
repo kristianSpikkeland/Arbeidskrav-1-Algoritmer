173f9d6768ba4153e273246f4df6380e797c13ef
Kristian <kristian.spikkeland@fagskoleniagder.no>
2026-09-05 20:02:40 +0200

    Added running of analyze

---
+++ KodeAlgo/KodeAlgo/Program.cs
@@ -0,0 +1,17 @@
+﻿using Algoritmer;
+using KodeAlgo;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Oppgave1.RunAnalyzeLinearAndBinary();
+
+       
+    }
+
+}
+
+
+
