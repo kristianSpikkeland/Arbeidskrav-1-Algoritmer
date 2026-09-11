06fcb1a6c4f5e5e84c77c11dee5dabad80cfe649
Kristian <kristian.spikkeland@fagskoleniagder.no>
2026-09-11 13:03:39 +0200

    Used dependency injection to move some spesific  graph implementation to a menu helper class.

---
+++ KodeAlgo/KodeAlgo/Menu/MenuOppgave4.cs
@@ -0,0 +1,47 @@
+﻿using KodeAlgo.Oppgave4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using KodeAlgo.Oppgave4;
+
+namespace KodeAlgo.Menu
+{
+    public class MenuOppgave4
+    {
+        private readonly BFSGraph _bfsGraph;
+        
+        public MenuOppgave4(BFSGraph bfsGraph)
+        {
+            _bfsGraph = bfsGraph;
+        }
+
+        public void BuildGraph()
+        {           
+            _bfsGraph.AddConnection("Majorstuen", "Nationaltheatret");
+            _bfsGraph.AddConnection("Nationaltheatret", "Stortinget");
+            _bfsGraph.AddConnection("Stortinget", "Jernbanetorget");
+            _bfsGraph.AddConnection("Jernbanetorget", "Grønland");
+            _bfsGraph.AddConnection("Grønland", "Tøyen");
+            _bfsGraph.AddConnection("Majorstuen", "Blindern");
+            _bfsGraph.AddConnection("Blindern", "Forskningsparken");
+            _bfsGraph.AddConnection("Forskningsparken", "Ullevål stadion");     
+        }
+
+        public void TraverseFromMajorstuen()
+        {
+            var order = _bfsGraph.TraverseBFS(_bfsGraph._adj, "Majorstuen");
+            Console.WriteLine(string.Join(", ", order));
+        }
+
+        public void ShortestPathMajorstuenGrønland()
+        {
+            var shortestPath = _bfsGraph.GetShortestPath("Majorstuen", "Grønland");
+
+            Console.WriteLine(string.Join(", ", shortestPath));
+            Console.WriteLine($"Shortest number of stops is {shortestPath.Count - 1}");
+            Console.WriteLine();
+        }
+
+        
+    }
+}
