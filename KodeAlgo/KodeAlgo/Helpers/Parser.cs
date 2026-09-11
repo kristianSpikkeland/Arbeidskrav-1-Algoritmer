42c340b145afec73e80d98b9dafffa4c3e56dd25
Kristian <kristian.spikkeland@fagskoleniagder.no>
2026-09-11 13:08:08 +0200

    Added simple string to int parser.

---
+++ KodeAlgo/KodeAlgo/Helpers/Parser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KodeAlgo.Helpers
+{
+    public static class Parser
+    {
+        public static int Int(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out int num))
+                {
+                    return num;
+                }
+                Console.WriteLine("Invalid input");
+            }
+           
+
+            
+        }
+    }
+}
