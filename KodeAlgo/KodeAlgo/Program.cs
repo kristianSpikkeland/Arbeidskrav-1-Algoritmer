acf47d4b330180f116e8927baf302f5c84d5f149
Kristian <kristian.spikkeland@fagskoleniagder.no>
2026-09-12 18:17:00 +0200

    Added case 5 DFS

--- KodeAlgo/KodeAlgo/Program.cs
+++ KodeAlgo/KodeAlgo/Program.cs
@@ -3,6 +3,7 @@ using KodeAlgo.Helpers;
 using KodeAlgo.Menu;
 using KodeAlgo.Oppgave2;
 using KodeAlgo.Oppgave4;
+using KodeAlgo.Oppgave5;
 using System.Collections;
 using System.Globalization;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -52,11 +53,28 @@ class Program
                         var menuOppgave4 = new MenuOppgave4(BFS);
 
                         menuOppgave4.BuildGraph();
-                        //menuOppgave4.TraverseFromMajorstuen();
+                        menuOppgave4.BFSTraverseFromMajorstuen();
                         menuOppgave4.ShortestPathMajorstuenGrønland();
+                        menuOppgave4.ShortestPathUllevålTøyen();
+
+
                         break;
                     }
 
+                case 5:
+                    {
+                        var DFS = new DFSGraph();
+                        var menuOppgave5 = new MenuOppgave5(DFS);
+
+                        menuOppgave5.BuildGraph();
+                        menuOppgave5.DFSTraverseRecursiveFromMajorstuen();
+                        menuOppgave5.DFSTraverseIterativeFromMajorstuen();
+                        var isPath = DFS.TryFindPath("Majorstuen", "Tøyn");
+                        Console.WriteLine(isPath);
+
+
+                        break;
+                    }
             }
 
         }
