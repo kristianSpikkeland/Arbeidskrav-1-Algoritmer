2588b68bd8d8001bb5f08a676fd9636da22474b7
Kristian <kristian.spikkeland@fagskoleniagder.no>
2026-09-17 15:50:29 +0200

    Added a menu helper class for queue testing

---
+++ KodeAlgo/KodeAlgo/Menu/MenuOppgave2.cs
@@ -0,0 +1,89 @@
+﻿using KodeAlgo.Helpers;
+using KodeAlgo.Oppgave2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KodeAlgo.Menu
+{
+    public static class MenuOppgave2
+    {
+        public static void QueueMenu()
+        {
+            var queue = new CustomQueue<int>(5);
+
+            while (true)
+            {
+                Console.WriteLine("""
+                1. Add integer to queue (Enqueue)
+                2. Remove (Dequeue)
+                3. Show queue
+                4. Show the first element in queue
+                5. Enrich with 5 elements
+                6. Show queue Size property and IndexFront properties
+                100. Return to main menu
+                """);
+                Console.WriteLine();
+
+                int choice = Parser.Int("Your choice: ");
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            int enqueueChoice = Parser.Int("Integer to add: ");
+                            Console.WriteLine();
+
+                            queue.Enqueue(enqueueChoice);
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            var removed = queue.Dequeue();
+                            Console.WriteLine($"{removed} was removed");
+                            Console.WriteLine();
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            queue.Print();
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            queue.Peek();
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 5:
+                        {
+                            queue.Enqueue(3);
+                            queue.Enqueue(7);
+                            queue.Enqueue(4);
+                            queue.Enqueue(6);
+                            queue.Enqueue(2);
+                            break;
+                        }
+
+                    case 6:
+                        {
+                            Console.WriteLine($"Size: {queue.Size}");
+                            Console.WriteLine($"IndexFront index: {queue.IndexFront}");
+                            Console.WriteLine($"IndexFront value: {queue._array[queue.IndexFront]}");
+                            break;
+                        }
+
+
+                    case 100:
+                        {
+                            return;
+                        }
+                }
+            }
+        }
+    }
+}
