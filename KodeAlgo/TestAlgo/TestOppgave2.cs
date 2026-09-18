2b9640859cc89dfd06e70ceacd7b1c4f90af6cbf
Kristian <kristian.spikkeland@fagskoleniagder.no>
2026-09-18 12:30:19 +0200

    Added CustomQueue tests

---
+++ KodeAlgo/TestAlgo/TestOppgave2.cs
@@ -0,0 +1,74 @@
+﻿using KodeAlgo;
+using KodeAlgo.Menu;
+using KodeAlgo.Oppgave2;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAlgo
+{
+    public class TestOppgave2
+    {
+
+        [Fact]
+        public void Dequeue_RemovingFromEmptyArray_ShouldThrowException()
+        {
+            // Arrange
+            var queue = new CustomQueue<int>();
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => queue.Dequeue());
+        }
+
+
+        [Fact]
+        public void Enqueue_AddingElementToFullArray_ShouldExpandArray()
+        {
+            // Arrange
+            // Creates a queue with 5 slots in array
+            var queue = new CustomQueue<int>(5);
+
+            // Act
+            // Adding 5 elements
+            queue.Enqueue(3);
+            queue.Enqueue(7);
+            queue.Enqueue(4);
+            queue.Enqueue(6);
+            queue.Enqueue(2);
+
+            // Adding element number 6 should double array  
+            queue.Enqueue(9);
+
+            // Assert
+            Assert.Equal(10, queue.Size);
+        }
+
+        [Fact]
+        public void Dequeue_RemovingElement_ShouldRemoveEmptySpace()
+        {
+            // Arrange
+            // Creates a queue with 5 slots in array
+            var queue = new CustomQueue<int>(5);
+
+            // Act
+            // Adding 5 elements
+            queue.Enqueue(3);
+            queue.Enqueue(7);
+            queue.Enqueue(4);
+            queue.Enqueue(6);
+            queue.Enqueue(2);
+
+            // Adding element number 6 sets queue Size to 10  
+            queue.Enqueue(9);
+
+ 
+            queue.Dequeue();
+
+            // Assert
+            Assert.Equal(5, queue.Size);
+        }
+
+
+    }
+}
